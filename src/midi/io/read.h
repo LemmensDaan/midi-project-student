1ae8a3cc6a44cc68561b3a159a8f82f105992271
Daan <daan.lemmens@hotmail.com>
2020-01-16 00:22:19 +0100

    read_array

--- src/midi/io/read.h
+++ src/midi/io/read.h
@@ -17,12 +17,27 @@ namespace io
 	}
 
 	// read a single T from in and return it
+	// more user - friendly version of read_to for reading a single value
 	template<typename T, typename std::enable_if<std::is_fundamental<T>::value, T>::type* = nullptr>
 	T read(std::istream& in) {
 		T read;
 		io::read_to(in, &read);
 		return read;
 	}
+
+	// more user - friendly version of read_to for reading multiple values
+	// returning an array
+	template<typename T>
+	std::unique_ptr<T[]> read_array(std::istream& in, size_t n = 1) {
+		std::unique_ptr<T[]> pointer = std::make_unique<T[]>(n);
+		for (int i = 0; i < n; i++) {
+			T read;
+			in.read(reinterpret_cast<char*> (&read), sizeof(T));
+			pointer[i] = read;
+		}
+		return pointer;
+	}
+
 }
 
 #endif

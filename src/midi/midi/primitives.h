20b84950ea5eac9d741fd5ad73ca8bc09c87e96d
Daan <daan.lemmens@hotmail.com>
2020-01-16 16:05:17 +0100

    note_number

--- src/midi/midi/primitives.h
+++ src/midi/midi/primitives.h
@@ -2,14 +2,20 @@
 
 namespace midi {
 	// Channels
-	// new type channel that only supports == and !=
+	// new type channel that only supports == and !=, (also << after channel show)
 	struct __declspec(empty_bases)Channel : tagged<uint8_t, Channel>, equality<Channel>, show_value<Channel, int> {
 		using tagged::tagged;
 	};
 
 	// Instruments
-	// supports == and != and <<
+	// new type of instrument that supports ==, != and <<
 	struct __declspec(empty_bases)Instrument : tagged<uint8_t, Instrument>, equality<Instrument>, show_value<Instrument, int> {
+		using tagged::tagged;
+	};
+
+	// Note numbers
+	// new type of note numbers that supports ==, !=, <, >, <=, >=, <<
+	struct __declspec(empty_bases)NoteNumber : tagged<uint8_t, NoteNumber>, ordered<NoteNumber>, show_value<NoteNumber, int> {
 		using tagged::tagged;
 	};
 }

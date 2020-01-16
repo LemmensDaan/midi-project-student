ccc068f48ca7f1ac6d7ef6ae9ffb075fa6eaa47b
Daan <daan.lemmens@hotmail.com>
2020-01-16 16:00:57 +0100

    instruments

--- src/midi/midi/primitives.h
+++ src/midi/midi/primitives.h
@@ -2,8 +2,14 @@
 
 namespace midi {
 	// Channels
-	// new type channel that onbly supports == and !=
+	// new type channel that only supports == and !=
 	struct __declspec(empty_bases)Channel : tagged<uint8_t, Channel>, equality<Channel>, show_value<Channel, int> {
+		using tagged::tagged;
+	};
+
+	// Instruments
+	// supports == and != and <<
+	struct __declspec(empty_bases)Instrument : tagged<uint8_t, Instrument>, equality<Instrument>, show_value<Instrument, int> {
 		using tagged::tagged;
 	};
 }

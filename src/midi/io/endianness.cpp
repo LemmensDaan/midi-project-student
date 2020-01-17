5f5095b4976914e393e69d93a8f5997ebd7a1829
Daan <daan.lemmens@hotmail.com>
2020-01-17 17:10:09 +0100

    consistency hexadecimal

--- src/midi/io/endianness.cpp
+++ src/midi/io/endianness.cpp
@@ -9,23 +9,23 @@ void io::switch_endianness(uint16_t* n) {
 }
 void io::switch_endianness(uint32_t* n) {
 	uint32_t  mirror = ((*n >> 24)) |
-		((*n << 8) & 0xff0000) |
-		((*n >> 8) & 0xff00) |
-		((*n << 24) & 0xff000000);
+		((*n << 8) & 0xFF0000) |
+		((*n >> 8) & 0xFF00) |
+		((*n << 24) & 0xFF000000);
 	*n = mirror;
 }
 void io::switch_endianness(uint64_t* n) {
 	uint64_t m1 = (*n >> 32);
 	uint64_t mirror_m1 = ((m1 >> 24)) |
-		((m1 << 8) & 0xff0000) |
-		((m1 >> 8) & 0xff00) |
-		((m1 << 24) & 0xff000000);
+		((m1 << 8) & 0xFF0000) |
+		((m1 >> 8) & 0xFF00) |
+		((m1 << 24) & 0xFF000000);
 	uint64_t m2 = (*n << 32);
 	uint64_t m2a = (m2 >> 32);
 
 	uint64_t mirror_m2a = ((m2a >> 24)) |
-		((m2a << 8) & 0xff0000) |
-		((m2a >> 8) & 0xff00) |
-		((m2a << 24) & 0xff000000);
+		((m2a << 8) & 0xFF0000) |
+		((m2a >> 8) & 0xFF00) |
+		((m2a << 24) & 0xFF000000);
 	*n = (mirror_m2a << 32) | mirror_m1;
 }
